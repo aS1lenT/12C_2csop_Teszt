2765fea757b85c7bb8edd427385573b3fa6c7600
aS1lenT <tothmartin06@gmail.com>
2024-10-02 11:01:25 +0200

    KorHenger elkezdve

---
+++ KorHenger/Kor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KorHenger
+{
+    class Kor
+    {
+        //Az osztály feladata egy kör területének és kerületének kiszámítása
+
+        //Osztályváltozók
+        private double sugar, kerület, terület;
+
+        //Konstruktorok
+        public Kor() { }
+        public Kor(double r)
+        {
+
+        }
+    }
+}
