79d326c76372e308ce2c3fcf750f1f3837908811
aS1lenT <tothmartin06@gmail.com>
2024-10-02 10:41:42 +0200

    test0

--- Teszt_UniTeszt/UnitTest1.cs
+++ Teszt_UniTeszt/UnitTest1.cs
@@ -10,14 +10,17 @@ namespace Teszt_UniTeszt
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange: Beállítások és várt eredmény megadása
             string vartEredm = "hello vilag";
 
+            //Act: Tesztelt metódus meghívása
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
                 Teszt.Program.Main();
                 var kapottEredm = sw.ToString().Trim();
 
+                //Assert: Kiértékelés
                 Assert.AreEqual(vartEredm, kapottEredm);
                 
             }
