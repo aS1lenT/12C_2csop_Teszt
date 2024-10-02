eede127256e3ba73a70a73d2383fabdd1e2aa841
aS1lenT <tothmartin06@gmail.com>
2024-10-02 10:31:32 +0200

    dawcdfewa

---
+++ Teszt_UniTeszt/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Teszt_UniTeszt
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string vartEredm = "hello vilag";
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Teszt.Program.Main();
+                var kapottEredm = sw.ToString().Trim();
+
+                Assert.AreEqual(vartEredm, kapottEredm);
+                
+            }
+        }
+    }
+}
