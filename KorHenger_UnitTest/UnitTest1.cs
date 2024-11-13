11dc03ddd96e75fca5ce98ec4a751ff273a5da5a
aS1lenT <tothmartin06@gmail.com>
2024-11-13 11:55:39 +0100

    20241113

--- KorHenger_UnitTest/UnitTest1.cs
+++ KorHenger_UnitTest/UnitTest1.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using KörHenger;
 
 namespace KorHenger_UnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class Kör_Teszt
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Kerület_Teszt()
+        {
+            // Arrange - Feltételek beállítása
+            double sugár = 5;
+            Kör kör = new Kör(sugár);
+            kör.SetKerület();
+            double 
+            // Act - Végrehajtás
+
+            // Assert - Kiértékelés
+        }
+
+        [TestMethod]
+        public void Terület_teszt()
         {
         }
     }
